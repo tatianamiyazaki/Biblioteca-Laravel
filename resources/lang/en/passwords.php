113f63c629b7b30b47b643334ff9d3a0cb4f41ba
Tatiana Hitomi Miyazaki <tatimiyazaki@gmail.com>
2016-09-16 19:36:56 -0300

    login pronto

---
+++ resources/lang/en/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Senha deve ter pelo menos seis dígitos e ser igual à confirmação.',
+    'reset' => 'Sua senha foi resetada!',
+    'sent' => 'Encaminhado e-mail com link para resetar sua senha!',
+    'token' => 'Código inválido para resetar senha.',
+    'user' => "Nenhum usuário encontrado para este e-mail",
+
+];
---
+++ resources/views/home.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.app')
+
+@section('content')
+<style>
+    html, body {
+        background: url('/pergaminho.jpg') no-repeat fixed center center;
+        background-color: #fff;
+        color: white;
+        font-family: 'Cursive', serif;
+        font-weight: 100;
+        height: 100vh;
+        margin: 0;
+    }
+
+    .full-height {
+        height: 100vh;
+    }
+
+    .flex-center {
+        align-items: center;
+        display: flex;
+        justify-content: center;
+    }
+
+    .position-ref {
+        position: relative;
+    }
+
+    .top-right {
+        position: absolute;
+        right: 10px;
+        top: 18px;
+    }
+
+    .content {
+        text-align: center;
+    }
+
+    .title {
+        font-size: 84px;
+    }
+
+    .links > a {
+        color: white;
+        padding: 0 25px;
+        font-size: 20px;
+        font-weight: 600;
+        letter-spacing: .1rem;
+        text-decoration: none;
+        text-transform: uppercase;
+    }
+
+    .m-b-md {
+        margin-bottom: 30px;
+    }
+</style>
+
+<div class="container">
+    
+</div>
+@endsection
